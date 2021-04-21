f89f9acf4b6f5447b0b308b4727b9c09427cb960
Deivanai08 <78594650+Deivanai08@users.noreply.github.com>
2021-04-21 19:01:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ exports.config = {
 //  sanity3: 'test/specs/sanity3/*.spec.js',
 
     userOrgAndCredentials: 'test/specs/userOrgAndCredentials/*.spec.js',
-    collaborationAndInteraction: 'test/specs/collaborationAndInteraction/*.spec.js',
-    sourcing: 'test/specs/sourcing/*.spec.js',
-    Consumption: 'test/specs/Consumption/*.spec.js',
+   // collaborationAndInteraction: 'test/specs/collaborationAndInteraction/*.spec.js',
+   // sourcing: 'test/specs/sourcing/*.spec.js',
+   // Consumption: 'test/specs/Consumption/*.spec.js',
     // contentcreation: 'test/specs/content-creation/*.spec.js',
 //QuestionScenrio: 'test/specs/QuestionScenrio-CC/*.spec.js',
   // tpd: 'test/specs/tpd/*.spec.js', 

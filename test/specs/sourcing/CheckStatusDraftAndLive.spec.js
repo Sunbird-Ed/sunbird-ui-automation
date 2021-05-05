342d83922e0c945b009bb59aa4593d66345e995a
binayakdash123 <64831476+binayakdash123@users.noreply.github.com>
2021-05-05 13:24:47 +0530

    Update CheckStatusDraftAndLive.spec.js

--- test/specs/sourcing/CheckStatusDraftAndLive.spec.js
+++ test/specs/sourcing/CheckStatusDraftAndLive.spec.js
@@ -21,6 +21,7 @@ describe('CheckStatusDraftAndLive', () => {
         browser.manage().deleteAllCookies();
     });
     it('CheckStatusDraftAndLive',function(){
+        utility.handleDropDown();
         utility.handleLocationPopup();
         browser.sleep(3000);
         utility.userLogin('Creator');

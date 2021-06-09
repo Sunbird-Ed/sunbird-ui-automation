84d4cef4914aed30cbcd7ef87f3563172cf5bdf4
Deivanai08 <78594650+Deivanai08@users.noreply.github.com>
2021-06-09 09:48:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -106,7 +106,7 @@ exports.config = {
         version: '78.0.3904.108',
         platform: 'Any',
         shardTestFiles: true,
-        maxInstances:3,
+        maxInstances:2,
         chromeOptions: {
             prefs: {
                 profile: {

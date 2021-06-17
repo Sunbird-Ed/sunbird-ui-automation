a2b29c495b8e86cc493efb48d35e28f68f8001e8
Deivanai08 <78594650+Deivanai08@users.noreply.github.com>
2021-06-17 18:06:58 +0530

    Update changePath.js

--- test/pathFolder/changePath.js
+++ test/pathFolder/changePath.js
@@ -3,15 +3,15 @@ var ConfigurePath = () => {
 //var excelSheetPath ='C:/Jenkinspath/testdatafiles/testDataSheetPreprod.xls';
 //var excelSheetPath ='C:/Jenkinspath/testdatafiles';
   //var excelSheetPath ='./test/testdatafiles/testDataSheetProd.xls';
-//var excelSheetPath ='./test/testdatafiles/testDataSheetPreprod.xls';
-var excelSheetPath ='./test/testdatafiles/testDataSheetStagingEnv.xls';
+var excelSheetPath ='./test/testdatafiles/testDataSheetPreprod.xls';
+//var excelSheetPath ='./test/testdatafiles/testDataSheetStagingEnv.xls';
 
 
 
 // var AppURL='https://diksha.gov.in';
-//var AppURL='https://preprod.ntp.net.in';
+var AppURL='https://preprod.ntp.net.in';
 //var AppURL='https://staging.ntp.net.in';
- var AppURL = 'https://staging.sunbirded.org';
+ //var AppURL = 'https://staging.sunbirded.org';
 
  // var AppURL='https://staging.diksha.gov.in';
 

541ee7f94f5787a7ac7ec04135511593633283cb
Deivanai08 <78594650+Deivanai08@users.noreply.github.com>
2021-07-05 12:37:21 +0530

    Update changePath.js

--- test/pathFolder/changePath.js
+++ test/pathFolder/changePath.js
@@ -10,8 +10,8 @@ var excelSheetPath ='./test/testdatafiles/testDataSheetStagingEnv.xls';
 
 // var AppURL='https://diksha.gov.in';
 //var AppURL='https://preprod.ntp.net.in';
-var AppURL='https://staging.ntp.net.in';
-// var AppURL = 'https://staging.sunbirded.org';
+//var AppURL='https://staging.ntp.net.in';
+ var AppURL = 'https://staging.sunbirded.org';
 
  // var AppURL='https://staging.diksha.gov.in';
 

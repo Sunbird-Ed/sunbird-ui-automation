3ae8e9b653c589226c35f92bd84ab307447abeb0
raju <raju.s@qualitrix.com>
2023-05-18 20:18:33 +0530

    Profile

---
+++ test/specs/Profile/FillUserIDSecWithSpaceAndSubmitInProfile.spec.js
@@ -0,0 +1,27 @@
+const utility = require(protractor.basePath + '/test/utility/utilityFunctions.js');
+const usrOnBoardfun = require(protractor.basePath + '/test/pageObject/UserOnBoardingPageObj.js');
+let getAppURL=require(protractor.basePath + '/test/pathFolder/changePath.js');
+
+describe('validateSpaceAllowedInEnterUserIDSectionInUpdateFormOfProfileSection', () => {
+
+    beforeEach(() => {
+        browser.ignoreSynchronization = true;
+        var Url=getAppURL.ConfigurePath().AppURL;
+        var AppendExplore='/explore';
+        browser.get(Url+AppendExplore, 40000);
+        browser.manage().timeouts().implicitlyWait(30000);
+        browser.driver.manage().window().maximize(); 
+    });
+
+    afterEach(() => {
+        browser.waitForAngularEnabled(false);
+        browser.manage().deleteAllCookies();
+    });
+    
+    it('FillUserIDSecWithSpaceAndSubmitInProfile',function(){
+        utility.handleDropDown();
+        utility.handleLocationPopup();
+        utility.userLogin('AddUserCred');
+        usrOnBoardfun.enterSpaceInUserIDInProfile();
+    });
+});

640f8fc50ef398e93fa54f86fd768d8dc59ab122
raju <raju.s@qualitrix.com>
2023-08-08 20:19:24 +0530

    Login

---
+++ test/specs/CourseConsumptionP1/VerifyCollapseAllOption.spec.js
@@ -0,0 +1,30 @@
+const { browser } = require("protractor");
+
+const utility = require(protractor.basePath + '/test/utility/utilityFunctions.js');
+const verifyCEBpageobj = require(protractor.basePath+'/test/pageObject/VerifySignInPopupInExploreCourseEnrollButtonObj.js');
+let getAppURL=require(protractor.basePath + '/test/pathFolder/changePath.js');
+const searchCBFPPageObj = require(protractor.basePath+'/test/pageObject/tpdPageObj.js');
+
+describe('VerifyCollapseAllOption', () => {
+
+    beforeEach(() => {
+        browser.ignoreSynchronization = true;
+        var Url=getAppURL.ConfigurePath().AppURL;
+        var AppendExplore='/explore';
+        browser.get(Url+AppendExplore, 40000);
+        browser.manage().timeouts().implicitlyWait(30000);
+        browser.driver.manage().window().maximize(); 
+    });
+
+    afterEach(() => {
+        browser.waitForAngularEnabled(false);
+        browser.manage().deleteAllCookies();
+    });
+
+    it('VerifyCollapseAllOption',function(){
+        utility.handleDropDown();
+                utility.handleLocationPopup();
+        utility.userLogin('Tntenant');
+        searchCBFPPageObj.verifyCollapseAndExpandAllOption();
+    });
+});
---
+++ test/specs/GroupsP2/validateAdminAndNonAdminMemberIsAbleToViewCourseProgressOfActivityInGroup.spec.js
@@ -0,0 +1,44 @@
+const { browser } = require("protractor");
+
+const utility = require(protractor.basePath + '/test/utility/utilityFunctions.js');
+let getAppURL = require(protractor.basePath + '/test/pathFolder/changePath.js');
+const tpdPageObj = require(protractor.basePath + '/test/pageObject/tpdPageObj.js');
+const lspPageObj = require(protractor.basePath + '/test/pageObject/lessonPlanPageObj.js');
+const sanityfun = require(protractor.basePath + '/test/pageObject/SanityPageObj.js');
+
+describe('validateAdminAndNonAdminMemberIsAbleToViewCourseProgressOfActivityInGroup', () => {
+
+    beforeEach(() => {
+        browser.ignoreSynchronization = true;
+        var Url = getAppURL.ConfigurePath().AppURL;
+        var AppendExplore = '/explore';
+        browser.get(Url + AppendExplore, 40000);
+        browser.manage().deleteAllCookies();
+        browser.manage().timeouts().implicitlyWait(30000);
+        browser.driver.manage().window().maximize();
+    });
+
+    afterEach(() => {
+        browser.waitForAngularEnabled(false);
+        browser.manage().deleteAllCookies();
+    });
+    
+    it('validateAdminAndNonAdminMemberIsAbleToViewCourseProgressOfActivityInGroup', function () {
+        utility.handleDropDown();
+        utility.handleLocationPopup();
+        utility.userLogin('ContentCreator');
+        let data = tpdPageObj.createCourseAndReturnDoidAndCourseName();
+        let course = data.courseName;
+        let urldata = data.currentUrl;
+        urldata.then(function (input) {
+        var doid = input.split("/")[6];
+        utility.userLogout();
+        utility.userLogin('ContentReviewer');
+        tpdPageObj.publishCourseFromUpForReview2(course);
+        utility.userLogout();
+        utility.userLogin('NewCredCourseToc');
+        sanityfun.verifyAddActivitySearchedByKeywordInCourseBucket(course);
+        sanityfun.clickOnActivityInGroupAndValidateProgressBar();
+    })
+  })
+});
